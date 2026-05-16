8aa224251cc8a4a2cfd2cf5b95fb1dc3a6019fb5
brahmika <brahmika30@gmail.com>
2026-05-16 15:08:43 +0530

    [Brahmika] Added: CartRepository with findByUserId and deleteByUserId

--- backend/src/main/java/com/bookstore/modules/cart/repository/CartRepository.java
+++ backend/src/main/java/com/bookstore/modules/cart/repository/CartRepository.java
@@ -6,14 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-/**
- * Repository for Cart entity
- * 
- * TODO: Implement the following methods:
- * - Optional<Cart> findByUserId(Long userId)
- * - void deleteByUserId(Long userId)
- */
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    // TODO: Add custom query methods
+
+    Optional<Cart> findByUserId(Long userId);
+
+    void deleteByUserId(Long userId);
 }
